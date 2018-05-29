87bc3bbeebfeace1c47f280e43f1d1cde886f2f8
Shivam Mathur <shivam_jpr@hotmail.com>
2018-05-29 15:31:57 +0530

    Update data and add detect geocoding fails

--- data/data.py
+++ data/data.py
@@ -1,3 +1,4 @@
+import os
 import json
 import time
 import geocoder
@@ -13,16 +14,24 @@ with open('se.json', encoding="utf-8-sig") as data_file:
 data = []
 
 for i in oh:
-	ctry = None
-	while(ctry is None):
-		ctry = geocoder.google(i["mentee"]['time_zone_str'].split('(')[0].strip()).country_long
-	data.append({"user": abs(hash(str(i["mentee"]['id']))), "country": ctry})
+    ctry = None
+    while True:
+        ctry = geocoder.google(i["mentee"]['time_zone_str'].split('(')[0].strip())
+        if not ctry.error:
+            break;
+    if not ctry.country_long:
+        print("no country", str(i["mentee"]['time_zone_str'].split('(')[0].strip()))
+    data.append({"user": abs(hash(str(i["mentee"]['id']))), "country": ctry.country_long})
 
 for i in se:
-	ctry = None
-	while(ctry is None):
-		ctry =geocoder.google(i["mentee"]['timezone_display'].split('(')[0].strip()).country_long
-	data.append({"user": abs(hash(str(i['id']))), "country": ctry})
+    ctry = None
+    while True:
+        ctry = geocoder.google(i["mentee"]['timezone_display'].split('(')[0].strip())
+        if not ctry.error:
+            break;        
+    if not ctry.country_long:
+        print("no country", str(i["mentee"]['timezone_display'].split('(')[0].strip()))        
+    data.append({"user": abs(hash(str(i['id']))), "country": ctry.country_long})
 
 with open('data.json', 'w') as outfile:
     json.dump(data, outfile)
